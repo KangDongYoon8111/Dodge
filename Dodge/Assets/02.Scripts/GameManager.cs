0b1637ca84390ed9aee890c07cfdea59451bab8a
강동윤 <davidkang8111@gmail.com>
2022-09-23 12:34:40 +0900

    20220923_01

--- Dodge/Assets/02.Scripts/GameManager.cs
+++ Dodge/Assets/02.Scripts/GameManager.cs
@@ -9,10 +9,10 @@ public class GameManager : MonoBehaviour
 {
     // 게임오버 시 활성화할 텍스트 게임 오브젝트
     public GameObject gameoverText;
-    // 생존 시간을 표시할 텍스트 컴포넌트
-    public Text timeText;
-    // 최고 기록을 표시할 텍스트 컴포넌트
-    public Text recordText;
+    // 생존 시간을 표시할 TextMeshPro 컴포넌트
+    public TextMeshProUGUI timeText;
+    // 최고 기록을 표시할 TextMeshPro 컴포넌트
+    public TextMeshProUGUI recordText;
 
     private float surviveTime; // 생존 시간
     private bool isGameover; // 게임오버 상태
@@ -26,12 +26,32 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        // 게임오버가 아닌 동안
+        if (!isGameover)
+        {
+            // 생존 시간 갱신
+            surviveTime += Time.deltaTime;
+            // 갱신한 생존 시간을 timeText
+            // TextMeshPro 컴포너트를 이용해 표시
+            timeText.text = "Time : " + (int)surviveTime;
+        }
+        else
+        {
+            // 게임오버 상태에서 R키를 누른다면,
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                // MainScene 씬을 로드
+                SceneManager.LoadScene(0);
+            }
+        }
     }
 
     // 현재 게임을 게임오버 상태로 변경하는 메서드
     public void EndGame()
     {
-
+        // 현재 상태를 게임오버 상태로 전환
+        isGameover = true;
+        // 게임오버 텍스트 게임 오브젝트를 활성화
+        gameoverText.SetActive(true);
     }
 }
