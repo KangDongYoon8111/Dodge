82f29fc71284efc357ffd051fe34932a908e3cc0
강동윤 <davidkang8111@gmail.com>
2023-04-27 12:38:32 +0900

    20230427

---
+++ Dodge/Assets/01.Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI; // UI 관련 라이브러리
+using UnityEngine.SceneManagement; // 씬 관리 관련 라이브러리
+
+public class GameManager : MonoBehaviour
+{
+    // 게임오버 시 활성화할 텍스트 게임 오브젝트
+    public GameObject gameoverText;
+    // 생존 시간을 표시할 텍스트 컴포넌트
+    public Text timeText;
+    // 최고 기록을 표시할 텍스트 컴포넌트
+    public Text recordText;
+
+    private float surviveTime; // 생존시간
+    private bool isGameover; // 게임오버 상태
+
+    void Start()
+    {
+        // 생존 시간과 게임오버 상태 초기화
+        surviveTime = 0;
+        isGameover = false;
+    }
+
+    void Update()
+    {
+        // 게임오버가 아닌 동안
+        if (!isGameover)
+        {
+            // 생존 시간 갱신
+            surviveTime += Time.deltaTime;
+            // 갱신한 생존 시간을 timeText
+            // 텍스트 컴포넌트를 이용해 표시
+            timeText.text = "Time : " + (int)surviveTime;
+        }
+        else
+        {
+            // 게임오버 상태에서 R 키를 누른 경우
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                // SampleScene 씬을 로드
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    // 현재 게임을 게임오버 상태로 변경하는 메서드
+    public void EndGame()
+    {
+        // 현재 상태를 게임오버 상태로 전환
+        isGameover = true;
+        // 게임오버 텍스트 게임 오브젝트를 활성화
+        gameoverText.SetActive(true);
+    }
+}
